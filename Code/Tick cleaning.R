d73377e9cda62314e1a4a5de354d9a8f107f70a6
Nosa Osawe <osaweemmanuelnosa@gmail.com>
2024-08-12 14:37:26 +0100

    updates

---
+++ Code/Tick cleaning.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+tick <- read.csv("C:\\Users\\DELL\\Documents\\Git in R\\Ticks\\Data\\Tick_working_sheet.csv")
+view(tick)
+
+# Replace all NA values with 0 using 
+Tick <- replace(tick, is.na(tick), 0)
+str(Tick)
+
+sum(is.na(Tick))  # Nice and clean
